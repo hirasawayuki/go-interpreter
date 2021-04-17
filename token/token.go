0fbde028ee96e970ad248ed0a7af59b2d4f14116
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2021-04-17 16:56:15 +0900

    add token keywords

--- token/token.go
+++ token/token.go
@@ -7,6 +7,18 @@ type Token struct {
 	Literal string
 }
 
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIndent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
+
 const (
 	// トークン文字が未知
 	ILLEGAL = "ILLEGAL"

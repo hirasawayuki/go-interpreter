697a6234b1e04caa6b39f0130e5e52a400e9b97d
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2021-04-17 17:49:15 +0900

    add keyword token

--- token/token.go
+++ token/token.go
@@ -8,8 +8,13 @@ type Token struct {
 }
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 func LookupIndent(ident string) TokenType {
@@ -51,4 +56,9 @@ const (
 	// キーワード
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )

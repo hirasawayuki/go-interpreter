a821312e35d672b6ac31fbb601057fdb499cbb2c
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2021-04-25 18:03:54 +0900

    Add Boolean expression

--- ast/ast.go
+++ ast/ast.go
@@ -149,3 +149,12 @@ type Identifier struct {
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 func (i *Identifier) String() string       { return i.Value }
+
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
+func (b *Boolean) expressionNode()      {}
+func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
+func (b *Boolean) String() string       { return b.Token.Literal }
